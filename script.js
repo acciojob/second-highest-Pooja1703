e85e0fa0cc6de3e91eebf1c3edf869ef79e642b6
Pooja1703 <129085948+Pooja1703@users.noreply.github.com>
2026-09-15 20:02:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+  
+  if (arr.length <= 1) {
+    return -Infinity;
+  }
+
+  arr.sort((a, b) => b - a);
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] != arr[0]) {
+      return arr[i];
+    }
+  }
+
+  return -Infinity;
+}
